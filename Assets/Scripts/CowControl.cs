2b0bef1e13cb9ddcba99cb9f39aca5eb6a7f0c24
Hannah Craven <22901091+hannahcraven@users.noreply.github.com>
2020-04-28 17:54:35 -0700

    Added cactus, changed camera view

---
+++ Assets/Scripts/CowControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowControl : MonoBehaviour
+{
+    public float speed = 20f;
+    public float turnspeed = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Make cow spawn
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make cow run into distance (other side of screen)
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Cow"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //Make cow zig zag while moving forward
+     transform.Translate(Vector3.forward * speed * Time.deltaTime);
+}
