f1a4877771fc2c7086e14ea54477f28f5471ad40
Hannah Craven <22901091+hannahcraven@users.noreply.github.com>
2020-05-09 13:15:45 -0700

    add Score Board

--- Assets/Scripts/CowControl.cs
+++ Assets/Scripts/CowControl.cs
@@ -6,12 +6,24 @@ public class CowControl : MonoBehaviour
 {
     public float speed = 20f;
     public float turnspeed = 8.0f;
+    private float spawnRate = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         //Make cow spawn
+        StartCoroutine(SpawnCows());
 
+    }
+
+    IEnumerator SpawnCow()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, Cows.Count);
+            Instantiate(cows[index]);
+        }
     }
 
     // Update is called once per frame
@@ -19,6 +31,10 @@ public class CowControl : MonoBehaviour
     {
         //Make cow run into distance (other side of screen)
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        //Make cow zig zag while moving forward
+        transform.Translate(Vector3.left * turnspeed * Time.deltaTime);
+        transform.Translate(Vector3.right * turnspeed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -28,7 +44,4 @@ public class CowControl : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    //Make cow zig zag while moving forward
-     transform.Translate(Vector3.forward * speed * Time.deltaTime);
 }
